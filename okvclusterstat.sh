f19fd7eff3b8b1e652ffb91528d4d832cd0db3d5
frankvdborden <120718283+frankvdborden@users.noreply.github.com>
2024-07-26 17:33:48 +0200

    Delete okvclusterstat.sh

--- okvclusterstat.sh
+++
@@ -1,3 +0,0 @@
-. okvenv.sh
-#okv cluster info get|jq '.value.nodes[] | "\(.nodeID),\(.nodeName) ,\(.status), \(.mode)"'|sed 's/"//g'
-okv cluster info get|jq '.value.nodes[] | "\(.nodeName),\(.nodeID) ,\(.ipAddress), \(.mode),\(.status),\(.readWritePeer),\(.clusterSubgroup),\(.joinDate),\(.disableDate)"'|sed 's/"//g'
