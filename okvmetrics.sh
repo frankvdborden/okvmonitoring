c0166a18bd46f78d44393aaec3e92acee0e749b3
frankvdborden <120718283+frankvdborden@users.noreply.github.com>
2024-07-26 17:33:10 +0200

    Delete okvmetrics.sh

--- okvmetrics.sh
+++
@@ -1,5 +0,0 @@
-. okvenv.sh
-for i in `cat /home/oracle/bin/profiles`
-do
-	echo "$i," `okv server info get --profile $i|jq '.value | "\(.caCertificateExpirationDate),\(.cpuCores) ,\(.diskInGB), \(.fraInGB), \(.memoryInKB), \(.platformCertificatesExpirationDate), \(.serverCertificateExpirationDate), \(.version)"'|sed 's/"//g'`
-done
