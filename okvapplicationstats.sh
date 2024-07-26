b8fdf6dacb05b4835b6a9adbe6751e10458d7087
frankvdborden <120718283+frankvdborden@users.noreply.github.com>
2024-07-26 17:33:59 +0200

    Delete okvapplicationstats.sh

--- okvapplicationstats.sh
+++
@@ -1,7 +0,0 @@
-. okvenv.sh
-FROM=`date --date=' 1 minutes ago'  +"%Y-%m-%d %H:%M:%S"`
-#FROM=`date +"%Y-%m-%d %H:%M:%S"`
-for i in `cat /home/oracle/bin/profiles`
-do
-	okv metrics application get --profile $i --start-time "$FROM" --end-time NOW --include ALL --interval PT15M --statistic MEAN
-done
